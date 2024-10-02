a2adf95dad46e6ce9c9abf53f6e28f780576c097
Corrine01 <corrinekariuki@gmail.com>
2024-10-02 10:32:08 +0300

    learning python

---
+++ hello.py
@@ -0,0 +1,26 @@
+print ("hellow world")
+
+x= 76
+q= "risper"
+print(x)
+print("my name is :",q)
+
+print (len(q))
+print(q.upper())
+print(2)
+
+#if condition statement
+a=37
+c=498
+if  c>a :
+   print("c is greater than a")
+else:
+    print("c is less than a")
+    
+    #age for voting
+age=int (input("enter your age"))
+if age >=18:
+    print("you can vote")
+else :
+    print("you are a minor")
+
